6bc4933b7cb8925110ba13b7869abf0664f6ff2e
Warren Shenk <4896526+wshenk@users.noreply.github.com>
2023-06-04 15:27:42 -0400

    added header processing

--- ultibust.py
+++ ultibust.py
@@ -9,38 +9,49 @@ import warnings
 from urllib.parse import urlparse
 from concurrent.futures import ThreadPoolExecutor
 import logging
+import threading
 
 output_csv_fields = ['host', 'path', 'method', 'resp_status_code', 'resp_content_length']
+output_file_lock = threading.Lock()
 
 
 def main():
+    global http_headers
+
     args = parse_arguments()
     setup_logging(args.logfile, args.debug)
 
     hosts = parse_newline_delimited_file(args.hosts_file)
     paths = parse_newline_delimited_file(args.paths_file)
+    http_headers = parse_header_file(args.header_file)
     http_methods = ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'CONNECT', 'TRACE']
     methods_and_urls = combine_hosts_paths_and_methods(hosts, paths, http_methods)
 
     output_filename = create_output_file(args.output_file)
 
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        for result in executor.map(dirb_url_request, methods_and_urls):
-            write_result_to_output_file(result, output_filename)
-            
+    with open(output_filename, 'a') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=output_csv_fields)
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for result in executor.map(dirb_url_request, methods_and_urls):
+                with output_file_lock:
+                    writer.writerow(result)
+
+    logging.info("Completed ultibust, results: {}".format(output_filename))
 
 
 def dirb_url_request(method_and_url):
+    global http_headers
+
     method = method_and_url["method"]
     url = method_and_url["url"]
-    print(url)
     url_p = urlparse(url)
     host = url_p.hostname
     path = url_p.path
     status_code = -1
     content_length = -1
     try:
-        response = requests.request(method, url, allow_redirects=False)
+        response = requests.request(method, url, allow_redirects=False, headers=http_headers)
+        logging.debug(response.request.headers)
         status_code = response.status_code
         content_length = len(response.content)
     except (requests.exceptions.ConnectionsError, requests.exceptions.ReadTimeout, requests.exceptions.TooManyRedirects, requests.excpetion.RequestException) as e:
@@ -91,6 +102,21 @@ def parse_newline_delimited_file(filename):
     items = [item.strip() for item in items]
     return items
 
+def parse_header_file(filename):
+    header_dict = {}
+    if filename:
+        with open(filename, 'r') as header_file:
+            headers = header_file.readlines()
+        headers = [header.strip() for header in headers]
+        for header in headers:
+            if ":" in header:
+                hlist = header.split(":")
+                header_name = hlist[0].strip()
+                header_value = hlist[1].strip()
+                header_dict[header_name] = header_value
+    return header_dict
+
+
 
 def create_output_file(output_file_arg):
     filename = ""
@@ -103,12 +129,6 @@ def create_output_file(output_file_arg):
         writer.writeheader()
     logging.info("Created output file {}".format(filename))
     return filename
-
-def write_result_to_output_file(result, output_file):
-    with open(output_file, 'a') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=output_csv_fields)
-        writer.writerow(result)
-
 
 
 def setup_logging(logfile_arg, debug_arg):
